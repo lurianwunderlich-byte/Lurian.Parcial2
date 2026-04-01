53dd826f8ef1ef24384d4cda704197de86c9fd5c
lurianwunderlich-byte <lurian.wunderlich@escola.pr.gov.br>
2026-04-01 16:57:13 -0300

    Create desafio3.py

---
+++ desafio3.py
@@ -0,0 +1,8 @@
+# Programa para criar uma lista de 5 nomes e imprimir na tela
+
+# Criando uma lista com 5 nomes
+nomes = ["Ana", "Bruno", "Carlos", "Daniela", "Eduardo"]
+
+# Percorrendo a lista e imprimindo cada nome
+for nome in nomes:
+    print(nome)
