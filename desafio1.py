e43d964f842aca10256dd26a4c63b2ca6484cbf8
lurianwunderlich-byte <lurian.wunderlich@escola.pr.gov.br>
2026-04-01 16:56:13 -0300

    Add program to sum two numbers

---
+++ desafio1.py
@@ -0,0 +1,13 @@
+# Programa para somar dois números
+
+# Pedindo ao usuário para digitar o primeiro número
+num1 = float(input("Digite o primeiro número: "))
+
+# Pedindo ao usuário para digitar o segundo número
+num2 = float(input("Digite o segundo número: "))
+
+# Realizando a soma dos dois números
+soma = num1 + num2
+
+# Exibindo o resultado na tela
+print("A soma dos números é:", soma)
